e9f7c54952339782237d7711f1e702252913de2b
Thomas Schwappach <Thomas.schwappach@gmail.com>
2015-08-26 10:08:40 -0600

    1 question done ish

--- week 2/src/modulusProject.java
+++ week 2/src/modulusProject.java
@@ -1,11 +1,17 @@
-
+import java.util.Scanner;
 public class modulusProject
 	{
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				Scanner userImput= new Scanner(System.in);
+				System.out.println("Dear user how many questions do you want to answer between 1-10?");
+				int numberOfQuestions = userImput.nextInt();
+				if (numberofQuestions==1)
+					{
+						System.out.println("1. what is "+(((int)(Math.random()*10)+5)%((int)(Math.random()*5)))+"?");					
+					}
+				
 			}
 
 	}
