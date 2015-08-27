03e984db40a8f69c8d7fe2614b90231a536e8b1a
Thomas Schwappach <Thomas.schwappach@gmail.com>
2015-08-27 08:04:53 -0600

    finished

--- week 2/src/modulusProject.java
+++ week 2/src/modulusProject.java
@@ -1,17 +1,72 @@
 import java.util.Scanner;
+
 public class modulusProject
 	{
-
-		public static void main(String[] args)
+		static Scanner userInput = new Scanner(System.in);
+		public static void main(String[] args) 
 			{
-				Scanner userImput= new Scanner(System.in);
-				System.out.println("Dear user how many questions do you want to answer between 1-10?");
-				int numberOfQuestions = userImput.nextInt();
-				if (numberofQuestions==1)
-					{
-						System.out.println("1. what is "+(((int)(Math.random()*10)+5)%((int)(Math.random()*5)))+"?");					
-					}
+				System.out.print("How many questions would you like to do? ");
+				int numberOfQuestions = userInput.nextInt();
+				int questions = ((int)(Math.random() * (numberOfQuestions - 1)));
+				int correct = 0;
 				
+				for (int i = 0 ; i < numberOfQuestions; i++)
+					{
+						int a = ((int)(Math.random() * 20));
+						int b = ((int)(Math.random() * 10) + 1);
+					
+						if (i == questions)
+							{
+								int answer = a%b;
+								
+								System.out.print(a + " % " + b + " =");
+								int guess = userInput.nextInt();
+								
+								if (guess == answer)
+									{
+										System.out.println("Nice");
+										correct++;
+									}
+								else
+									{
+										System.out.println("Nope, wrong, The Correct Answer is: " + answer);
+									}	
+							}
+						else if ((i + 1) == questions)
+							{
+								int answer = a % a;
+								
+								System.out.print(a + " % " + a + " =");
+								int guess = userInput.nextInt();
+								
+								if(guess == answer)
+									{
+										System.out.println("Nice");
+										correct++;
+									}	
+								else
+									{
+										System.out.println("Nope, wrong, The Correct Answer is: " + answer);
+									}
+							}
+						else
+							{
+								int answer = b % a;
+								
+								System.out.print(b + " % " + a + " =");
+								int guess = userInput.nextInt();
+								
+								if(guess == answer)
+									{
+										System.out.println("Nice");
+										correct++;
+									}	
+								else
+									{
+										System.out.println("Nope, wrong, The Correct Answer is: " + answer);
+									}
+							}
+					}
+				System.out.println("You got " + correct + " out of " + numberOfQuestions + " correct");
 			}
-
-	}
+}
